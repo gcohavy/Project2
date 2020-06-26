a050bf6c15ad7ca72c153c7e70bf60009db2a033
Gabriel Cohavy <gcohavy@gmail.com>
2020-06-26 15:00:25 -0700

    added search component, ERD, and wireframe

---
+++ Project 2/force-app/main/default/aura/PlayerPokemon/PlayerPokemonController.js
@@ -0,0 +1,17 @@
+({
+	doInit : function(cmp) {
+        //console.log('Initialization begun');
+		let action = cmp.get('c.getPlayerPokemon');
+        action.setParams({ UserId: $A.get('$SObjectType.CurrentUser.Id')});
+        action.setCallback(this, (res) => {
+            if(res.getState() == 'SUCCESS') {
+            	//console.log('Successful status')
+            	cmp.set('v.Pokemon', res.getReturnValue());
+        	} else {
+                console.log('Could not return nothin');
+            }
+        });
+		$A.enqueueAction(action);
+		//console.log('Initialization complete');
+	}
+})
